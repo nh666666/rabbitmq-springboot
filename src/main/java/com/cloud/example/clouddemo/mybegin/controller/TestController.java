6389fcf426ec3134a4cb115d18c6a505508160d0
niehan <niehan@ebupt.com>
2020-10-16 16:38:17 +0800

    [fix]: 修改9

--- src/main/java/com/cloud/example/clouddemo/mybegin/controller/TestController.java
+++ src/main/java/com/cloud/example/clouddemo/mybegin/controller/TestController.java
@@ -24,6 +24,6 @@ public class TestController {
     @GetMapping(value = "/testHello")
     @ApiOperation(value = "测试")
     public String testHello() {
-        return "helloworld++++23456789";
+        return "helloworld++++2345678910";
     }
 }
